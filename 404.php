801163e68f791c40b4bfd0e8961b9c7c0d87e080
kravchenko06 <marikkrava@gmail.com>
2024-06-02 00:03:09 +0200

    404

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+include('other/header.php');
+?> 
+<main>
+      
+      <section class="container">
+        <div class="row">
+          <div class="col-100 text-center">
+              <h1>404 not found</h1>
+               
+          </div>
+        </div>
+      </section>
+    </main>
+    
+<?php
+    include_once('other/footer.php')
+?> 
